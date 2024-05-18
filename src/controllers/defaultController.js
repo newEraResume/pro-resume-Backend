ce8711dd28f336f630a7ee99d5614d886f306d24
RIshabh Rai <rishabh.rai@quaeris.ai>
2024-05-19 01:58:01 +0530

    trying

--- src/controllers/defaultController.js
+++ src/controllers/defaultController.js
@@ -1,7 +1,7 @@
 
 const defaultController = async (req, res) => {
   try {
-    res.json({ message:"Hello" });
+    res.json({ message:"Hello" , test:"new" });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
