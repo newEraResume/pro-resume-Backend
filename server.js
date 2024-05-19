76f251feb392d67b37c8e5c6f4c6ba90887aa6c8
RIshabh Rai <rishabh.rai@quaeris.ai>
2024-05-19 12:08:14 +0530

    Handled the cors error

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./src/routes');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use('/', routes);
 
