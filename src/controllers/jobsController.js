62640713730ac18de118a6ebf8139e7f4fb93ea6
RIshabh Rai <rishabh.rai@quaeris.ai>
2024-05-22 11:19:47 +0530

    Added the jobs api and fetched the details

---
+++ src/controllers/jobsController.js
@@ -0,0 +1,14 @@
+const { getJobs } = require('../services/jobsServce');
+
+const jobsController = async (req, res) => {
+  try {
+    let { prompt } = req.body;
+    const jobs = await getJobs(prompt);
+    res.json({ jobs });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = { jobsController };
