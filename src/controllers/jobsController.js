77267e5d3b3e6a40f79380b76110a36ca1fabc7c
RIshabh Rai <rishabh.rai@quaeris.ai>
2024-05-22 11:24:33 +0530

    Added Software developer jobs as default prompt

--- src/controllers/jobsController.js
+++ src/controllers/jobsController.js
@@ -3,6 +3,9 @@ const { getJobs } = require('../services/jobsServce');
 const jobsController = async (req, res) => {
   try {
     let { prompt } = req.body;
+    if(!prompt){
+        prompt = 'Software developer jobs';
+    }
     const jobs = await getJobs(prompt);
     res.json({ jobs });
   } catch (error) {
