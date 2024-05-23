80d7cfd3b5fa2f2549333ff88d53b69d448c4eaa
RIshabh Rai <rishabh.rai@quaeris.ai>
2024-05-24 00:37:35 +0530

    Added validation for users in db

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,82 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  contact: Object,
-  education: Object,
-  experience: Object,
-  projects: Object,
-  skills:String,
-  summary:String,
-  involvements:String
-});
+const arrayMinLengthValidator = function (value) {
+  return value && value.length > 0;
+};
 
+const userSchema = new Schema({
+  form_details: {
+    personal_details: {
+      name:{
+        type:String,
+        required:true
+      },
+      email:{
+        type:String,
+        required:true
+      },
+      phone:{
+        type:Number,
+        required:true
+      },
+      state:{
+        type:String,
+        required:false
+      },
+      city:{
+        type:String,
+        required:false
+      },
+      github:{
+        type:String,
+        required:false
+      },
+      linkdin:{
+        type:String,
+        required:false
+      },
+      others:{
+        type:String,
+        required:false
+      }
+    },
+    education: {
+      type: [Object],
+      validate: [arrayMinLengthValidator, 'At least one education detail is required'],
+      required: true
+    },
+    work_experience: {
+      type: [Object],
+      required: false
+    },
+    projects: {
+      type: [Object],
+      required: false
+    },
+    skills: {
+      type: [String],
+      validate: [arrayMinLengthValidator, 'At least one education detail is required'],
+      required: true
+    },
+    summary: {
+      type: String,
+      required: true
+    },
+    involvements: {
+      type: String,
+      required: false
+    }
+  },
+  templateid: {
+    type: [Number],
+    validate: [arrayMinLengthValidator, 'At least one template ID is required']
+  },
+  applied_jobs: [Object],
+  emailId: {
+    type: String,
+    unique: true,
+    required: true
+  }
+});
 module.exports = mongoose.model("User", userSchema);
