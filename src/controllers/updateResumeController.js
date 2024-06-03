2ebc7966a9cb5913bbcac39affdbda6132216742
RIshabh Rai <rishabh.rai@quaeris.ai>
2024-06-03 23:31:46 +0530

    Modified the resumeupdate api

--- src/controllers/updateResumeController.js
+++ src/controllers/updateResumeController.js
@@ -1,9 +1,23 @@
 const { enhancePrompt } = require('../services/openAiService');
+const  ResumeDetails  = require('../models/ResumeDetails'); // Assuming you have a ResumeDetails model
 
 const updateResumeController = async (req, res) => {
   try {
-    let { formdetails , job_description , required_skills } = req.body;
-    let prompt = `I am updating my resume so please Modify skills and summary in form dtails object and return only updated form details as response based on the provide job description and required skills . Please keep the key of form details objects unhindered and only updated the required fields like skills , summary etc - formdetails - ${formdetails} jobdescription-${job_description} ${ required_skills ? 'required skills' - required_skills : ''}`;
+    const { emailId, job_description, required_skills } = req.body;
+    const resumeDetails = await ResumeDetails.findOne({ emailId: emailId });
+    if (!resumeDetails) {
+      return res.status(404).json({ error: 'User Not Found' , status:404 });
+    }
+    const formdetails = resumeDetails?.form_details;
+    let prompt = `
+      I am updating my resume so please modify skills and summary in the form details object by suggesting more meaningful skills and summary based on the job_description.Please suggest at least 3-4 extra skills
+      and return only updated form details as response based on the provided job description and required skills.
+      Please keep the keys of form details objects unchanged and only update the required fields like skills, summary, etc.
+      formdetails - ${JSON.stringify(formdetails)}
+      jobdescription - ${job_description}
+      ${required_skills ? `required skills - ${required_skills}` : ''}
+    `;
+
     const completion = await enhancePrompt(prompt);
     res.json({ completion });
   } catch (error) {
